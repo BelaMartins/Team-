fdb973d077d84fc2389b1cb1e3ed424b3437f583
ericksoncomas <56272955+ericksoncomas@users.noreply.github.com>
2019-10-28 15:09:21 -0400

    sumary

---
+++ calcErickson.js
@@ -0,0 +1,8 @@
+function calcFinalAmount(){
+  D = Dollars.value  // read weight from element with id="dollars"
+  I = Interest.value  // read height from element with id="percentage"
+  finalamount = D*I+ D  // calculate the Interest
+  FinalAmount.innerHTML =    // write into label element with id ""
+      "Your Final Amount is "
+      +FinalAmount.toFixed(1)  // round BMI to 1 decimal place
+}
